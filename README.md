852cf1bbada7c17be81b472bef5aa230bfc2e057
brianca2013 <31380993+brianca2013@users.noreply.github.com>
2021-09-19 12:47:25 -0700

    Finalized a workable version for Markdown
    - Remove the original analytics code to avoid  data leaking
    - Set copy markdown as the default option
    -  Clean up the title for Google Apps links

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # chrome-copy-link
-Chrome extension to copy page url and title as a rich text link to easy paste it into a Google Doc.
-
-Published in chrome web store: https://chrome.google.com/webstore/detail/copy-page-link/ijkpnaifgolhlcfhcfamddnlnfjpjaji
+Chrome extension to copy page url and title as a rich text link, or markdown link. Forked from  https://github.com/Leksyk/chrome-copy-link
 
 ## Credits
 * Chrome toolbar icons taken from: https://www.iconfinder.com
--- copyLink.js
+++ copyLink.js
@@ -22,20 +22,16 @@ chrome.contextMenus.create({
 chrome.contextMenus.onClicked.addListener(function(info, tab) {
   switch (info.menuItemId) {
     case COPY_AS_PLAIN_MENU_ITEM_ID:
-      emitCopyEvent('copy-plain-text', 'menu');
-      copyTabLinkToClipboard(tab, false);
+      copyLinkAsPlainTextToClipboard(tab);
       break;
 
     case COPY_LINK_MENU_ITEM_ID:
-      emitCopyEvent('html-copy-link', 'menu');
-      copyTabLinkToClipboard(tab, true);
+      copyLinkToClipboard(tab);
       break;
 
     case COPY_AS_MARKDOWN_MENU_ITEM_ID:
-      emitCopyEvent('copy-markdown-text', 'menu');
-      copyTabLinkToClipboard(tab, true);
+      copyLinkAsMarkdownToClipboard(tab);
       break;
-
 
     default:
       throw Error('Unknown menu item' + info.menuItemId);
@@ -44,78 +40,31 @@ chrome.contextMenus.onClicked.addListener(function(info, tab) {
 
 // onClicked action gives us the tab without any permissions needed.
 chrome.browserAction.onClicked.addListener(function(tab) {
-  emitCopyEvent('html-copy-link', 'icon');
-  copyTabLinkToClipboard(tab, true);
+  copyLinkAsMarkdownToClipboard(tab);
 });
 
 chrome.commands.onCommand.addListener(function(command) {
   switch (command) {
     case 'copy-link':
-      emitCopyEvent('html-copy-link', 'shortcut');
       getActiveTab(function(tab) {
-        return copyTabLinkToClipboard(tab, true);
+        return copyLinkToClipboard(tab);
       });
       break;
 
     case 'copy-as-plain':
-      emitCopyEvent('copy-plain-text', 'shortcut');
       getActiveTab(function(tab) {
-        return copyTabLinkToClipboard(tab, False);
+        return copyLinkAsPlainTextToClipboard(tab);
       })
 
     case 'copy-as-markdown':
-      emitCopyEvent('copy-markdown-text', 'shortcut');
       getActiveTab(function(tab) {
-        return copyTabLinkToClipboard(tab, False);
+        return copyLinkAsMarkdownToClipboard(tab);
       })
 
     default:
       throw new Error('Unknown command: ' + command);
   }
 });
-
-function setupGoogleAnalytics() {
-  if (!window.ga) {
-    (function(){
-      window.ga = function() {
-        (window.ga.q = window.ga.q || []).push(arguments);
-      }, window.ga.l = 1 * new Date();
-      var tag = 'script';
-      var a = document.createElement(tag);
-      var m = document.getElementsByTagName(tag)[0];
-      a.async = 1;
-      a.src = 'https://www.google-analytics.com/analytics.js';
-      m.parentNode.insertBefore(a, m);
-    })();
-    ga('create', 'UA-61069739-1', 'auto');
-    ga('set', 'checkProtocolTask', null);
-  }
-}
-
-function analyticsEvent(category, action, opt_label, opt_value) {
-  console.log('ga', arguments);
-  // setupGoogleAnalytics();
-  ga('send', {
-      'hitType': 'event',
-      'eventCategory': category,
-      'eventAction': action,
-      'eventLabel': opt_label,
-      'eventValue': opt_value
-  });
-}
-
-// Number of copy actions made so far within this session.
-var numCopyActions = 0;
-
-function emitCopyEvent(method, source) {
-  numCopyActions += 1;
-  analyticsEvent('copy', method, source, numCopyActions);
-}
-
-function emitPermissionsEvent(permissions, granted) {
-  category = granted ? 'permissions-granted' : 'permissions-rejected';
-  analyticsEvent(category, permissions, permissions);
-}
 
 function getActiveTab(callback) {
   requestPermissions('tabs', function() {
@@ -137,10 +86,8 @@ function requestPermissions(permission, callback) {
     permissions: [permission]
   }, function(granted) {
     if (granted) {
-      emitPermissionsEvent(permission, true);
       callback();
     } else {
-      emitPermissionsEvent(permission, false);
       if (permission != "notifications") {
         showNotification('Error', 'Unable to get link: User rejected tabs permissions request.');
       } else {
@@ -162,18 +109,19 @@ function showNotification(title, text) {
   });
 }
 
-function copyTabLinkToClipboard(tab, as_html) {
-  var url = tab.url;
-  var title = tab.title;
-  console.log('Copying to clipboard', url, title);
-  if (as_html) {
-    copyLinkToClipboard(url, title);
-  } else {
-    copyLinkAsPlainTextToClipboard(url, title);
-  }
+function getUrl(tab) {
+  return tab.url;
 }
 
-function copyLinkAsPlainTextToClipboard(url, title) {
+function getTitle(tab) {
+  return tab.title.replace(/(- Google Docs|- Google Slides|- Google Sheets)$/, '');
+}
+
+function copyLinkAsPlainTextToClipboard(tab) {
+  var url = getUrl(tab);
+  var title = getTitle(tab);
+  console.log('Copying to clipboard', url, title);
+
   var span = document.createElement('span');
   span.innerText = title + ' ' + url;
   document.body.appendChild(span);
@@ -187,7 +135,29 @@ function copyLinkAsPlainTextToClipboard(url, title) {
   }
 }
 
-function copyLinkToClipboard(url, title) {
+function copyLinkAsMarkdownToClipboard(tab) {
+  var url = getUrl(tab);
+  var title = getTitle(tab);
+  console.log('Copying to clipboard', url, title);
+
+  var span = document.createElement('span');
+  span.innerText = '['+ title + '](' + url + ')';
+  document.body.appendChild(span);
+  try {
+    span.focus();
+    document.execCommand('SelectAll');
+    document.execCommand("Copy", false, null);
+    showNotification('Link in markdown format is ready to be pasted', '"' + title + '" was copied to clipboard');
+  } finally {
+    span.remove();
+  }
+}
+
+function copyLinkToClipboard(tab) {
+  var url = getUrl(tab);
+  var title = getTitle(tab);
+  console.log('Copying to clipboard', url, title);
+
   var link = document.createElement('a');
   link.innerText = title;
   link.setAttribute('href', url);
